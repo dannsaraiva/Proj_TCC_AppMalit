d267cc59d0990929b539575eeb800197afc03a3f
Daniel <daniel.saraiva.oliveira@outlook.com>
2023-07-11 22:53:47 -0300

    Pequenos ajustes de segurança.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.252.232:3333'
-    // baseURL: 'https://malitapi.azurewebsites.net'
-    // baseURL: 'https://webmalit.azurewebsites.net'
+    baseURL: 'http://192.168.81.232:3333'
 });
 
 export default api;
